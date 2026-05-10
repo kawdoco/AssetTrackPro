b4f9152a6c90030ec2b8748d1abd3549ed83ea21
isharamadushani1998 <ishararathnasekara1998@gmail.com>
2026-05-10 11:46:05 +0530

    Update index.ts

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import buildingReducer from './slices/buildingSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    buildings: buildingReducer,
   },
 });
 
@@ -11,5 +13,3 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
-
-
